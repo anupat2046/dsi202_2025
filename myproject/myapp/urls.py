c5fcc7f40550510c8ad8e282caede8069b066635
anupat2046 <suttilertanupat@gmail.com>
2025-05-13 22:23:38 +0700

    add path

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='myapp/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('signup/', views.signup, name='signup'),
+    path('accounts/', include('allauth.urls')),
 
     # API URLs
     path('api/', include(router.urls)),
